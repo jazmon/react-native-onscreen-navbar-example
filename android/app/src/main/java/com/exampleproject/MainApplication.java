84c3c66107aca96d8f02db1bb4ee866e8157ed85
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2018-02-22 15:52:13 +0100

    Run react-native-git-upgrade

--- android/app/src/main/java/com/exampleproject/MainApplication.java
+++ android/app/src/main/java/com/exampleproject/MainApplication.java
@@ -27,6 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
             new NavigationBarPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
